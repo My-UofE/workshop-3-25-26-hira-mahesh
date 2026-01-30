b497a47a848665ca9f71fb765405d273bd1c6485
hira-mahesh <mm1578@exeter.ac.uk>
2026-01-30 10:05:01 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -4,23 +4,21 @@ public class NumbersInSpanish {
        int number = Integer.parseInt(args[0]); 
  
        // delete the line below and replace with your code
-       if (number == 1) {
-         System.out.println("uno");
-        } 
-        else if (number == 2) {
-        System.out.println("dos");
-        }
-        else if ( number == 2 ) {
-        System.out.println("tres");
-        } 
-        else if ( number == 4) {
-        System.out.println("cuatro");
-        } 
-        else if ( number == 5) {
-        System.out.println("cinco");
-        } 
-        else {
-            System.out.println("not in valid range");
-        }
+    
+        // Using switch
+     switch(number) {
+        case 1:
+           System.out.println( "uno" ); break; // don't forget the break after each case!
+        case 2:
+           System.out.println( "dos" ); break;
+        case 3:
+           System.out.println( "tres" ); break; // don't forget the break after each case!
+        case 4:
+           System.out.println( "quatro" ); break;
+        case 5:
+           System.out.println( "cinco" ); break;
+        default: System.out.println( "not in valid range" );
+}
     }
+
  }
