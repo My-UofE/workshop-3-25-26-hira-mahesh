2bf96463541432e1a0a5168ba32672d313e64451
hira-mahesh <mm1578@exeter.ac.uk>
2026-01-30 09:58:47 +0000

    AgeToGroup completed with ternary operator

---
+++ AgeToGroup.java
@@ -0,0 +1,12 @@
+public class AgeToGroup {
+    public static void main(String[] args) {
+        int age = Integer.parseInt(args[0]);
+
+        int ageGroup = 1;
+    
+        ageGroup = (age<18)?0:1;
+        ageGroup = (age>65)?2:1;
+            
+        System.out.println("Age: " + age + ", Age Group: " + ageGroup);
+    }
+}
